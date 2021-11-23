2cdc7a6f82b31a8a8611915fcf7a15971cfd1341
MohamadReza Gholami <93978563+Mohgholamii@users.noreply.github.com>
2021-11-23 12:58:06 -0800

    Add files via upload

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+$con = mysql_connect("localhost","kidsplu1_filmk10","~e*f_FjGcu)S");
+
+mysql_query("SET character_set_results=utf8,character_set_client=utf8,character_set_connection=utf8, character_set_database=utf8,character_set_server=utf8",$con);
+
+mysql_select_db("kidsplu1_filmk10", $con)
+?>
